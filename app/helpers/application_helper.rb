023f084674f45f9786e517bfba4e61fdcb03561f
Michal Szmyd <michal.szmyd@railwaymen.org>
2019-10-16 15:39:47 +0200

    Devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  IMAGE_SIZE = 250
+
+  def url_for_variant
+    variant_path = '/storage/va/ri/' + image.variant(resize: IMAGE_SIZE).key
+
+    Rails.application.credentials.fetch(Rails.env.to_sym)[:host] + variant_path
+  end
 end
