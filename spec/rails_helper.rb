49bfd177b4dfa60e5ef956ac053432f0e07f8f90
Michal Szmyd <michal.szmyd@railwaymen.org>
2019-10-17 09:26:46 +0200

    set 100% coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../config/environment', __dir__)
 require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/spec/'
@@ -19,6 +15,11 @@ SimpleCov.start 'rails' do
   add_group 'Queries', 'app/queries'
 end
 SimpleCov.minimum_coverage 100
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../config/environment', __dir__)
+
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
